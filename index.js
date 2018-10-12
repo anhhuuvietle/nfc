e354ccf34c8dd6ba0d9df750576f942630cc5a7f
Lê Hữu Việt Anh <anhhvle@kms-technology.com>
2018-10-12 15:20:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 3002;
+app.listen(port);
+
+console.log(`Server listening on ${port}`);
