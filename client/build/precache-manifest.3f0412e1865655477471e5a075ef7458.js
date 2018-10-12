d3dfa48e0fb1db73ac1bc81cc572d6e2ab378bfd
Lê Hữu Việt Anh <anhhvle@kms-technology.com>
2018-10-12 15:29:03 +0700

    build

---
+++ client/build/precache-manifest.3f0412e1865655477471e5a075ef7458.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "db5d8c9e37e8361f2a5d23701aaf1482",
+    "url": "/static/media/img4.db5d8c9e.jpg"
+  },
+  {
+    "revision": "fa519758336db89612803194cd613f1c",
+    "url": "/static/media/img3.fa519758.jpg"
+  },
+  {
+    "revision": "3ae63b978522645e11824ce9069c5279",
+    "url": "/static/media/img2.3ae63b97.jpg"
+  },
+  {
+    "revision": "de7ffdd35e92bb386ad8bf2527e9f2fe",
+    "url": "/static/media/img1.de7ffdd3.jpg"
+  },
+  {
+    "revision": "461814812c1ec7023bb7bffdeaa77933",
+    "url": "/static/media/img.46181481.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77351504ca6dd8e7c31e",
+    "url": "/static/js/main.77351504.chunk.js"
+  },
+  {
+    "revision": "1cd938f0aee440b73899",
+    "url": "/static/js/1.1cd938f0.chunk.js"
+  },
+  {
+    "revision": "77351504ca6dd8e7c31e",
+    "url": "/static/css/main.799c8601.chunk.css"
+  },
+  {
+    "revision": "fb93008b70f06004f2e736345d6a2423",
+    "url": "/index.html"
+  }
+];
